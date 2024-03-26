668865b3ce9781fad01c39158146883e9f378d96
kanaheo <stalvede0421@gmail.com>
2024-03-26 15:39:23 +0900

    timer add function

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,84 +1,102 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  '25:00',
-                  style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 80,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Center(
-                child: IconButton(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                '$totalSeconds',
+                style: TextStyle(
                   color: Theme.of(context).cardColor,
-                  iconSize: 120,
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.play_circle_outline,
-                  ),
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Pomodors',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color,
-                              fontWeight: FontWeight.w600,
-                            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                iconSize: 120,
+                color: Theme.of(context).cardColor,
+                onPressed: onStartPressed,
+                icon: const Icon(Icons.play_circle_outline),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
                           ),
-                          Text(
-                            '0',
-                            style: TextStyle(
-                              fontSize: 58,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .displayLarge!
-                                  .color,
-                              fontWeight: FontWeight.w600,
-                            ),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            fontSize: 58,
+                            fontWeight: FontWeight.w600,
+                            color:
+                                Theme.of(context).textTheme.displayLarge!.color,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          )
+        ],
+      ),
+    );
   }
 }
