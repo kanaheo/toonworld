3d94c7edc153955a4cad4035c48b014a02ae1ccd
kanaheo <stalvede0421@gmail.com>
2024-04-05 22:16:17 +0900

    flutter practice end

--- lib/widgets/screens/webtoon_detail_sscreen.dart
+++ lib/widgets/screens/webtoon_detail_sscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonworld/models/webtoon_detail_model.dart';
 import 'package:toonworld/models/webtoon_episode_model.dart';
 import 'package:toonworld/services/api_service.dart';
@@ -24,12 +25,44 @@ class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
   // widget의 의미는 위에 WebtoonDetailScreen <-이 widget와 같다
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      prefs.setStringList('likedToons', []);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     webtoon = ApiService.getToonById(widget.id);
     episodes = ApiService.getLatestEpisodesById(widget.id);
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -40,6 +73,14 @@ class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
         elevation: 2,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: Icon(
+              isLiked ? Icons.favorite : Icons.favorite_outline_outlined,
+            ),
+          )
+        ],
         title: Text(
           widget.title,
           style: const TextStyle(
